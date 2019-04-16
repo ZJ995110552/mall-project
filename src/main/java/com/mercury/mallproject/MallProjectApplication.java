b40f48fbb9bbecf8ec3dd63feb85b6c1922c7395
ZJ995110552 <995110552@qq.com>
2019-04-16 20:40:56 +0800

    Test controller

--- src/main/java/com/mercury/mallproject/MallProjectApplication.java
+++ src/main/java/com/mercury/mallproject/MallProjectApplication.java
@@ -2,9 +2,19 @@ package com.mercury.mallproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class MallProjectApplication {
+
+	@ResponseBody
+	@RequestMapping("/index")
+	public String home(){
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MallProjectApplication.class, args);
