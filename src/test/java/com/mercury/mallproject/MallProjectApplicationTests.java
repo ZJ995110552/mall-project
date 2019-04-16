4b923342256428eaeb48a5d4bfc381f6209e7f44
ZJ995110552 <995110552@qq.com>
2019-04-16 23:08:58 +0800

    add multi environment and logback

--- src/test/java/com/mercury/mallproject/MallProjectApplicationTests.java
+++ src/test/java/com/mercury/mallproject/MallProjectApplicationTests.java
@@ -1,13 +1,30 @@
 package com.mercury.mallproject;
 
+import com.mysql.cj.log.LogFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MallProjectApplicationTests {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
+	@Test
+	public void testLog(){
+
+		logger.info("info");
+
+		logger.error("error");
+	}
+
 
 	@Test
 	public void contextLoads() {
