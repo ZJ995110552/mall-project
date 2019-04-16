b20c6fca020fbfe8f4c39320c5a3707b7f7d7daa
ZJ995110552 <995110552@qq.com>
2019-04-16 20:10:57 +0800

    project init

---
+++ src/main/java/com/mercury/mallproject/MallProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mercury.mallproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallProjectApplication.class, args);
+	}
+
+}
