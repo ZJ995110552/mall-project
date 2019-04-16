6ecc2cd11f4e44c5786d7cd1f1844d90965bb46d
ZJ995110552 <995110552@qq.com>
2019-04-17 00:38:45 +0800

    add dependency and druid config

---
+++ src/main/java/com/mercury/mallproject/common/config/DruidConfig.java
@@ -0,0 +1,104 @@
+package com.mercury.mallproject.common.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+public class DruidConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.initialSize}")
+    private int initialSize;
+
+    @Value("${spring.datasource.minIdle}")
+    private int minIdle;
+
+    @Value("${spring.datasource.maxActive}")
+    private int maxActive;
+
+    @Value("${spring.datasource.maxWait}")
+    private int maxWait;
+
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
+    private int timeBetweenEvictionRunsMillis;
+
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
+    private int minEvictableIdleTimeMillis;
+
+    @Value("${spring.datasource.validationQuery}")
+    private String validationQuery;
+
+    @Value("${spring.datasource.testWhileIdle}")
+    private boolean testWhileIdle;
+
+    @Value("${spring.datasource.testOnBorrow}")
+    private boolean testOnBorrow;
+
+    @Value("${spring.datasource.testOnReturn}")
+    private boolean testOnReturn;
+
+    @Value("${spring.datasource.filters}")
+    private String filters;
+
+    @Value("${spring.datasource.logSlowSql}")
+    private String logSlowSql;
+
+//    @Bean
+//    public ServletRegistrationBean druidServlet() {
+//        ServletRegistrationBean servlet = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+//        servlet.setName("druidStatViewServlet");
+//        servlet.addInitParameter("resetEnable", "false");
+//        return servlet;
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean filterRegistrationBean() {
+//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+//        filterRegistrationBean.setFilter(new WebStatFilter());
+//        filterRegistrationBean.addUrlPatterns("/*");
+//        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+//        return filterRegistrationBean;
+//    }
+
+    @Bean
+    public DataSource druidDataSource() {
+        DruidDataSource datasource = new DruidDataSource();
+        datasource.setUrl(dbUrl);
+//        logger.info(dbUrl);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+        datasource.setDriverClassName(driverClassName);
+        datasource.setInitialSize(initialSize);
+        datasource.setMinIdle(minIdle);
+        datasource.setMaxActive(maxActive);
+        datasource.setMaxWait(maxWait);
+        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        datasource.setValidationQuery(validationQuery);
+        datasource.setTestWhileIdle(testWhileIdle);
+        datasource.setTestOnBorrow(testOnBorrow);
+        datasource.setTestOnReturn(testOnReturn);
+        try {
+            datasource.setFilters(filters);
+        } catch (SQLException e) {
+//            logger.error("druid configuration initialization filter", e);
+        }
+        return datasource;
+    }
+}
--- src/test/java/com/mercury/mallproject/MallProjectApplicationTests.java
+++ src/test/java/com/mercury/mallproject/MallProjectApplicationTests.java
@@ -1,6 +1,6 @@
 package com.mercury.mallproject;
 
-import com.mysql.cj.log.LogFactory;
+import com.mercury.mallproject.common.config.DruidConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -9,25 +9,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.sql.DataSource;
+import java.sql.Connection;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MallProjectApplicationTests {
 
-	private Logger logger = LoggerFactory.getLogger(getClass());
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
-	@Test
-	public void testLog(){
+    @Autowired
+    private DruidConfig druidConfig;
 
-		logger.info("info");
+    @Test
+    public void testLog() {
 
-		logger.error("error");
-	}
+        logger.info("info");
+
+        logger.error("error");
+    }
+
+    @Test
+    public void testDruid() {
+        try (
 
 
-	@Test
-	public void contextLoads() {
-	}
+                Connection connection = druidConfig.druidDataSource().getConnection()
+
+
+
+        ) {
+        } catch (Exception e) {
+        }
+    }
+
+
+    @Test
+    public void contextLoads() {
+    }
 
 }
