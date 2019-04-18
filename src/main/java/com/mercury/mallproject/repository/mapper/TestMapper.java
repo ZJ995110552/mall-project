7f09f4b68285a2dde7176a26fe69b3fd389b4dcb
ZJ995110552 <995110552@qq.com>
2019-04-18 21:27:47 +0800

    test mybatis generator , Test entity

---
+++ src/main/java/com/mercury/mallproject/repository/mapper/TestMapper.java
@@ -0,0 +1,22 @@
+package com.mercury.mallproject.repository.mapper;
+
+import com.mercury.mallproject.domain.Test;
+import com.mercury.mallproject.domain.TestExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TestMapper {
+    int countByExample(TestExample example);
+
+    int deleteByExample(TestExample example);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+}
