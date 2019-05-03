412ac1706363584989b73eb58bf9e70453af624f
ZJ995110552 <402659408@qq.com>
2019-05-03 08:12:13 +0800

    repair log4j2 but NoClassDefFoundError: org/apache/log4j/Logger bug

--- src/main/java/com/mercury/mallproject/common/config/DruidConfig.java
+++ src/main/java/com/mercury/mallproject/common/config/DruidConfig.java
@@ -1,7 +1,13 @@
 package com.mercury.mallproject.common.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +16,7 @@ import java.sql.SQLException;
 
 @Configuration
 public class DruidConfig {
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Value("${spring.datasource.url}")
     private String dbUrl;
@@ -59,28 +66,27 @@ public class DruidConfig {
     @Value("${spring.datasource.logSlowSql}")
     private String logSlowSql;
 
-//    @Bean
-//    public ServletRegistrationBean druidServlet() {
-//        ServletRegistrationBean servlet = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
-//        servlet.setName("druidStatViewServlet");
-//        servlet.addInitParameter("resetEnable", "false");
-//        return servlet;
-//    }
-//
-//    @Bean
-//    public FilterRegistrationBean filterRegistrationBean() {
-//        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-//        filterRegistrationBean.setFilter(new WebStatFilter());
-//        filterRegistrationBean.addUrlPatterns("/*");
-//        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
-//        return filterRegistrationBean;
-//    }
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean servlet = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        servlet.setName("druidStatViewServlet");
+        servlet.addInitParameter("resetEnable", "false");
+        return servlet;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
 
     @Bean
     public DataSource druidDataSource() {
         DruidDataSource datasource = new DruidDataSource();
         datasource.setUrl(dbUrl);
-//        logger.info(dbUrl);
         datasource.setUsername(username);
         datasource.setPassword(password);
         datasource.setDriverClassName(driverClassName);
@@ -97,7 +103,7 @@ public class DruidConfig {
         try {
             datasource.setFilters(filters);
         } catch (SQLException e) {
-//            logger.error("druid configuration initialization filter", e);
+            logger.error("druid configuration initialization filter", e);
         }
         return datasource;
     }
