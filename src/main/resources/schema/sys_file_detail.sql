b72d9f582a93f7416ff587659309acb7baacad31
ZJ995110552 <402659408@qq.com>
2019-08-07 20:30:21 +0800

    modify about,add column

--- src/main/resources/schema/sys_file_detail.sql
+++ src/main/resources/schema/sys_file_detail.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50643
  File Encoding         : 65001
 
- Date: 07/08/2019 20:01:32
+ Date: 07/08/2019 20:29:28
 */
 
 SET NAMES utf8mb4;
@@ -30,6 +30,7 @@ CREATE TABLE `sys_file_detail` (
   `extention_name` varchar(50) DEFAULT NULL COMMENT '附件扩展名',
   `physical_name` varchar(300) DEFAULT NULL COMMENT '附件名（物理名）',
   `file_content` varchar(300) DEFAULT NULL COMMENT '文件内容',
+  `doc_server_id` bigint(40) DEFAULT NULL COMMENT '文件主机id',
   `doc_server_address` varchar(40) DEFAULT NULL COMMENT '文件主机地址',
   `server_group` varchar(20) DEFAULT NULL COMMENT '附件组',
   `server_root_dir` varchar(500) DEFAULT NULL COMMENT '文件绝对路径',
@@ -37,6 +38,7 @@ CREATE TABLE `sys_file_detail` (
   `creator_ip` varchar(40) DEFAULT NULL COMMENT '创建者ip',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `del_flag` varchar(2) DEFAULT NULL COMMENT '删除标识',
+  `file_size` bigint(40) DEFAULT NULL COMMENT '附件大小（KB）',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统附件表';
 
