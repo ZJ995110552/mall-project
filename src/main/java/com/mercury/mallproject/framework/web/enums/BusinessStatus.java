b209ee41972a7569c14e143b8373a211934080bb
ZJ995110552 <402659408@qq.com>
2019-08-11 15:48:40 +0800

    add ruoyi enums

---
+++ src/main/java/com/mercury/mallproject/framework/web/enums/BusinessStatus.java
@@ -0,0 +1,17 @@
+package com.mercury.mallproject.framework.web.enums;
+
+/**
+ * 操作状态
+ */
+public enum BusinessStatus
+{
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL,
+}
---
+++ src/main/java/com/mercury/mallproject/framework/web/enums/DataSourceType.java
@@ -0,0 +1,21 @@
+package com.mercury.mallproject.framework.web.enums;
+
+/**
+ * 数据源
+ */
+public enum DataSourceType
+{
+    /**
+     * 主库
+     */
+    MASTER,
+
+    /**
+     * 从库
+     */
+    SLAVE,
+    /**
+     * oracle
+     */
+    ORACLE
+}
