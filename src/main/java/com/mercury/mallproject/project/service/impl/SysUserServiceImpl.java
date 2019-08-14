cc9146544b48f6329522b800bf84ecaa6c715a60
ZJ995110552 <402659408@qq.com>
2019-08-14 22:39:40 +0800

    modify add user by auto id and password

--- src/main/java/com/mercury/mallproject/project/service/impl/SysUserServiceImpl.java
+++ src/main/java/com/mercury/mallproject/project/service/impl/SysUserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.mercury.mallproject.project.service.impl;
 
 import com.google.common.collect.Iterables;
+import com.mercury.mallproject.common.id.DefaultIdGenerator;
+import com.mercury.mallproject.common.id.IdGenerator;
 import com.mercury.mallproject.project.domain.SysUser;
 import com.mercury.mallproject.project.domain.SysUserExample;
 import com.mercury.mallproject.project.mapper.SysUserMapper;
 import com.mercury.mallproject.project.service.SysUserService;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,8 @@ import java.util.List;
 @Service
 public class SysUserServiceImpl implements SysUserService {
 
+    private IdGenerator idGenerator = DefaultIdGenerator.getInstance();
+
     @Autowired
     private SysUserMapper sysUserMapper;
 
@@ -21,7 +26,6 @@ public class SysUserServiceImpl implements SysUserService {
     public SysUser queryByUserName(String username) {
         SysUserExample sysUserExample = new SysUserExample();
         SysUserExample.Criteria criteria = sysUserExample.createCriteria();
-
         criteria.andUsernameEqualTo(username);
         return Iterables.getFirst(sysUserMapper.selectByExample(sysUserExample), null);
     }
@@ -37,23 +41,21 @@ public class SysUserServiceImpl implements SysUserService {
 
     @Override
     public void save(SysUser user) {
+        user.setId(idGenerator.generateId());
+        user.setPassword(DigestUtils.sha256Hex(user.getPassword()));
         sysUserMapper.insert(user);
-
     }
 
     @Override
     public void updateByUserId(SysUser user) {
         SysUserExample sysUserExample = new SysUserExample();
         SysUserExample.Criteria criteria = sysUserExample.createCriteria();
-
         criteria.andUserIdEqualTo(user.getUserId());
-
         sysUserMapper.updateByExample(user, sysUserExample);
     }
 
     @Override
     public void deleteBatch(List<String> userIds) {
-
         SysUserExample sysUserExample = new SysUserExample();
         SysUserExample.Criteria criteria = sysUserExample.createCriteria();
         criteria.andUserIdIn(userIds);
