d6d0db859a7106339905cefea91ab0303eaa9361
ZJ995110552 <402659408@qq.com>
2019-08-15 00:03:45 +0800

    generator Menu and role_menu

--- src/test/java/com/mercury/mallproject/GeneratorUserAboutTest.java
+++ src/test/java/com/mercury/mallproject/GeneratorUserAboutTest.java
@@ -1,12 +1,8 @@
 package com.mercury.mallproject;
 
 import com.mercury.mallproject.common.utils.DateTimeUtil;
-import com.mercury.mallproject.project.domain.SysRole;
-import com.mercury.mallproject.project.domain.SysRoleUser;
-import com.mercury.mallproject.project.domain.SysUser;
-import com.mercury.mallproject.project.service.SysRoleService;
-import com.mercury.mallproject.project.service.SysRoleUserService;
-import com.mercury.mallproject.project.service.SysUserService;
+import com.mercury.mallproject.project.domain.*;
+import com.mercury.mallproject.project.service.*;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
 import org.junit.Test;
@@ -22,8 +18,8 @@ import java.util.Random;
 @SpringBootTest
 @Slf4j
 public class GeneratorUserAboutTest {
-    
-    private static final int NUM = 20;
+
+    private static final int NUM = 4 * 5;
 
     @Autowired
     private SysUserService sysUserService;
@@ -33,6 +29,12 @@ public class GeneratorUserAboutTest {
 
     @Autowired
     private SysRoleUserService sysRoleUserService;
+
+    @Autowired
+    private SysMenuService sysMenuService;
+
+    @Autowired
+    private SysRoleMenuService sysRoleMenuService;
 
 
     @Test
@@ -94,20 +96,82 @@ public class GeneratorUserAboutTest {
         for (int i = 0; i < NUM; i++) {
             SysRoleUser sysRoleUser = new SysRoleUser();
 
-            int rand = new Random().nextInt(NUM-1);
-            int rand1 = new Random().nextInt(NUM-1);
+            int rand = new Random().nextInt(NUM - 1);
+            int rand1 = new Random().nextInt(NUM - 1);
 
             sysRoleUser.setRoleId(sysRoles.get(rand).getId());
             sysRoleUser.setUserId(sysUsers.get(rand1).getId());
 
             sysRoleUserService.save(sysRoleUser);
 
-            log.info("用户[{}]与角色[{}]绑定成功！", sysUsers.get(rand1).getId(),sysRoles.get(rand1).getId());
+            log.info("用户[{}]与角色[{}]绑定成功！", sysUsers.get(rand1).getId(), sysRoles.get(rand1).getId());
         }
     }
 
+    @Test
+    public void testAddMenu() {
+        // 生成主菜单
+        for (int i = 0; i < NUM / 5; i++) {
+            SysMenu sysMenu = new SysMenu();
+            sysMenu.setPid(0L);
+            sysMenu.setType(new Byte("0"));
+            sysMenu.setIcon("icon-setting");
+
+            sysMenuService.save(sysMenu);
+        }
+
+        List<SysMenu> sysMenus0 = sysMenuService.queryAll();
+
+        // 生成子菜单
+        for (int i = 0; i < NUM / 2; i++) {
+            int rand = new Random().nextInt(NUM / 5 - 1);
+            SysMenu sysMenu = new SysMenu();
+            sysMenu.setPid(sysMenus0.get(rand).getId());
+            sysMenu.setUrl("demo/test" + String.format("%03d", i));
+            sysMenu.setType(new Byte("0"));
+            sysMenu.setIcon("icon-team");
+            sysMenu.setPermissions("test:demo:menu" + String.format("%03d", i));
+            sysMenuService.save(sysMenu);
+        }
+
+        List<SysMenu> sysMenus1 = sysMenuService.queryAll();
+        // 生成按钮
+        for (int i = 0; i < NUM / 2; i++) {
+            int rand = new Random().nextInt(NUM / 5 - 1);
+            SysMenu sysMenu = new SysMenu();
+            sysMenu.setPid(sysMenus1.get(rand).getId());
+            sysMenu.setUrl("demo/test" + String.format("%03d", i));
+            sysMenu.setType(new Byte("1"));
+            sysMenu.setIcon("icon-mail");
+            sysMenu.setPermissions("test:demo:button" + String.format("%02d", i));
+            sysMenuService.save(sysMenu);
+        }
+
+    }
+
+    @Test
+    public void testAddRoleMenu() {
+        DateTime now = DateTime.now();
+
+        List<SysMenu> sysMenus = sysMenuService.queryAll();
+
+        List<SysRole> sysRoles = sysRoleService.queryAll();
 
 
+        for (int i = 0; i < NUM/5; i++) {
+            SysRoleMenu sysRoleMenu = new SysRoleMenu();
+
+            int rand = new Random().nextInt(NUM - 1);
+            int rand1 = new Random().nextInt(NUM - 1);
+
+            sysRoleMenu.setRoleId(sysRoles.get(rand).getId());
+            sysRoleMenu.setMenuId(sysMenus.get(rand1).getId());
+
+            sysRoleMenuService.save(sysRoleMenu);
+
+            log.info("角色[{}]与菜单[{}]绑定成功！", sysRoles.get(rand).getId(), sysMenus.get(rand1).getId());
+        }
+    }
 
 
 
