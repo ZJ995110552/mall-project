d69a9d710ff29d73ab4ccdb20caf3e4b2c111e6a
ZJ995110552 <402659408@qq.com>
2019-08-14 22:53:36 +0800

    add SysRoleService

---
+++ src/main/java/com/mercury/mallproject/project/service/SysRoleService.java
@@ -0,0 +1,7 @@
+package com.mercury.mallproject.project.service;
+
+import com.mercury.mallproject.project.domain.SysRole;
+
+public interface SysRoleService {
+    int save(SysRole sysRole);
+}
---
+++ src/main/java/com/mercury/mallproject/project/service/impl/SysRoleServiceImpl.java
@@ -0,0 +1,24 @@
+package com.mercury.mallproject.project.service.impl;
+
+import com.mercury.mallproject.common.id.DefaultIdGenerator;
+import com.mercury.mallproject.common.id.IdGenerator;
+import com.mercury.mallproject.project.domain.SysRole;
+import com.mercury.mallproject.project.mapper.SysRoleMapper;
+import com.mercury.mallproject.project.service.SysRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SysRoleServiceImpl implements SysRoleService {
+
+    private IdGenerator idGenerator = DefaultIdGenerator.getInstance();
+
+    @Autowired
+    private SysRoleMapper sysRoleMapper;
+
+    @Override
+    public int save(SysRole sysRole) {
+        sysRole.setId(idGenerator.generateId());
+        return sysRoleMapper.insert(sysRole);
+    }
+}
