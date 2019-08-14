c295ee8ede5cb1b53731f35fc98220308d8fd993
ZJ995110552 <402659408@qq.com>
2019-08-14 18:54:31 +0800

    generator SysRoleUser

---
+++ src/main/java/com/mercury/mallproject/project/domain/SysRoleUser.java
@@ -0,0 +1,55 @@
+package com.mercury.mallproject.project.domain;
+
+import java.util.Date;
+
+public class SysRoleUser {
+    private Long id;
+
+    private Long roleId;
+
+    private Long userId;
+
+    private Long creator;
+
+    private Date createDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Long creator) {
+        this.creator = creator;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
---
+++ src/main/java/com/mercury/mallproject/project/mapper/SysRoleUserMapper.java
@@ -0,0 +1,30 @@
+package com.mercury.mallproject.project.mapper;
+
+import com.mercury.mallproject.project.domain.SysRoleUser;
+import com.mercury.mallproject.project.domain.SysRoleUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SysRoleUserMapper {
+    int countByExample(SysRoleUserExample example);
+
+    int deleteByExample(SysRoleUserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SysRoleUser record);
+
+    int insertSelective(SysRoleUser record);
+
+    List<SysRoleUser> selectByExample(SysRoleUserExample example);
+
+    SysRoleUser selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") SysRoleUser record, @Param("example") SysRoleUserExample example);
+
+    int updateByExample(@Param("record") SysRoleUser record, @Param("example") SysRoleUserExample example);
+
+    int updateByPrimaryKeySelective(SysRoleUser record);
+
+    int updateByPrimaryKey(SysRoleUser record);
+}
