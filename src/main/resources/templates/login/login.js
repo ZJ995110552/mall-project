fc19e6b3bcd03fda70d1bc69b81332b1a06ef007
zhujie <995110552@qq.com>
2019-08-15 17:30:50 +0800

    modify login about

--- src/main/resources/templates/login/login.js
+++ src/main/resources/templates/login/login.js
@@ -1,3 +1,88 @@
+$(document).ready(function () {
+    layui.use(['form', 'layer'], function () {
+        var form = layui.form
+            , layer = layui.layer
+            , layedit = layui.layedit
+            , laydate = layui.laydate;
+        //日期
+        // laydate.render({
+        //     elem: '#date'
+        // });
+        // laydate.render({
+        //     elem: '#date1'
+        // });
+
+        //创建一个编辑器
+        // var editIndex = layedit.build('LAY_demo_editor');
+
+        //自定义验证规则
+        form.verify({
+            username : function (value) {
+                if (value.length < 7) {
+                    return '用户名不能为空且至少6位';
+                }
+            }
+            // , password: [
+            //     /^[\S]{6,12}$/
+            //     ,'密码必须6到12位，且不能出现空格'
+            // ]
+            // , captcha: function (value) {
+            //    if(value.length < 1){
+            //        return '验证码不能为空';
+            //    }
+            // }
+        });
+
+        //监听指定开关
+        // form.on('switch(switchTest)', function (data) {
+        //     layer.msg('开关checked：' + (this.checked ? 'true' : 'false'), {
+        //         offset: '6px'
+        //     });
+        //     layer.tips('温馨提示：请注意开关状态的文字可以随意定义，而不仅仅是ON|OFF', data.othis)
+        // });
+
+        //监听提交
+        form.on('submit(login)', function () {
+            var data = {};
+            data.username = $("input[name='username']").val();
+            data.password = $("input[name='password']").val();
+
+            $.ajax({
+                type: "POST",
+                url: "/sys/login",
+                data: data,
+                dataType: "json",
+                success: function (result) {
+                    console.info(result);
+                    if (result.code == 0) {//登录成功
+                        parent.location.href = '/index.html';
+                    } else {
+                        layer.msg(result.msg, {icon: 5});
+                        refreshCode();
+                    }
+                }
+            });
+
+
+
+
+            // layer.alert("测试");
+            // layer.alert(JSON.stringify(data.field), {
+            //     title: '最终的提交信息'
+            // });
+            // return false;
+        });
+
+    });
+});
+function refreshCode() {
+    var captcha = document.getElementById("captcha");
+    // var captcha1 = document.getElementById("captcha1");
+    captcha.src = "images/captcha.png?d=" + new Date().getTime();
+    // captcha1.src = "images/captcha.png?d=" + new Date().getTime();
+}
+
+
 // layui.config({
 //     base: "js/"
 // }).use(['form', 'layer'], function () {
@@ -68,60 +153,54 @@
 //     top.location.href = location.href;
 
 
+// $('.id-login').on('click', function () {
+//     var datas = "username=" + $("#username").val() + "&password=" + $("#password").val() + "&captcha=" + $("#captcha").val();
+//     console.log("");
+//     $.ajax({
+//         type: "POST",
+//         url: "/sys/login",
+//         data: datas,
+//         dataType: "json",
+//         success: function (result) {
+//
+//             if (result.code == 200) {//登录成功
+//
+//                 parent.location.href = '/index.html';
+//             } else {
+//                 alert(result.message);
+//                 parent.location.href = '/login.html';
+//                 // $.confirm({
+//                 //     title: '对话框',
+//                 //     content: '一些内容...',
+//                 //     buttons: {
+//                 //         confirm: {
+//                 //             text: '确认',
+//                 //             action: function(){
+//                 //                 $.alert('确认的!');
+//                 //             }
+//                 //         },
+//                 //         cancel: {
+//                 //             text: '关闭',
+//                 //             action: function(){
+//                 //                 $.alert('取消的!');
+//                 //             }
+//                 //         },
+//                 //         somethingElse: {
+//                 //             text: '其他',
+//                 //             btnClass: 'btn-blue',
+//                 //             keys: ['enter', 'shift'],
+//                 //             action: function(){
+//                 //                 $.alert('其他的选择？');
+//                 //             }
+//                 //         }
+//                 //     }
+//                 // });
+//                 // refreshCode();
+//             }
+//         }
+//     });
+//
+// });
 
-$('.id-login').on('click', function () {
-    var datas = "username=" + $("#username").val() + "&password=" + $("#password").val() + "&captcha=" + $("#captcha").val();
-    console.log("");
-    $.ajax({
-        type: "POST",
-        url: "/sys/login",
-        data: datas,
-        dataType: "json",
-        success: function (result) {
-
-            if (result.code == 200) {//登录成功
-
-                parent.location.href = '/index.html';
-            } else {
-                alert(result.message);
-                parent.location.href = '/login.html';
-                // $.confirm({
-                //     title: '对话框',
-                //     content: '一些内容...',
-                //     buttons: {
-                //         confirm: {
-                //             text: '确认',
-                //             action: function(){
-                //                 $.alert('确认的!');
-                //             }
-                //         },
-                //         cancel: {
-                //             text: '关闭',
-                //             action: function(){
-                //                 $.alert('取消的!');
-                //             }
-                //         },
-                //         somethingElse: {
-                //             text: '其他',
-                //             btnClass: 'btn-blue',
-                //             keys: ['enter', 'shift'],
-                //             action: function(){
-                //                 $.alert('其他的选择？');
-                //             }
-                //         }
-                //     }
-                // });
-                // refreshCode();
-            }
-        }
-    });
-
-});
 
 
-function refreshCode() {
-    var captcha = document.getElementById("captcha");
-    var captcha1 = document.getElementById("captcha1");
-    // captcha.src = "/captcha.jpg?t=" + new Date().getTime();
-    // captcha1.src = "/captcha.jpg?t=" + new Date().getTime();
-}
